2687d8d8323000240a13ed9cd5bc2aeccf11a72f
Vanshiljain <115866666+Vanshiljain@users.noreply.github.com>
2024-01-19 12:55:19 +0530

    Update auth.guard.ts

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,7 +7,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     const { req } = ctx.getContext();
-    const token = req.headers.authorization?.replace('Bearer ', '');
+    const token = req.headers.authorsization?.replace('Bearer ', '');
     req.token = token;
     return req;
   }
